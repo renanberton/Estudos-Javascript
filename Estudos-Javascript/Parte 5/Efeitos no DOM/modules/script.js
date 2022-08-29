7868306b061284d15b3066885752dc3720ff6c92
renanberton <renan_berton@yahoo.com.br>
2022-08-29 11:03:52 -0300

    Aulas de data set e modules

---
+++ Estudos-Javascript/Parte 5/Efeitos no DOM/modules/script.js
@@ -0,0 +1,16 @@
+/* MÓDULOS
+3 Motivos para dividirmos o projeto em módulos:
+
+Manutenção
+
+Dividir o código em diferentes arquivos com funções específicas (módulos) facilita a manutenção.
+
+Compartilhamento
+
+O compartilhamento de código com outros projetos é facilitado, pois basta você importar um módulo específico.
+
+Nativo no ES6+
+
+Ferramentas que permitem dividirmos o código em módulos já existem a bastante tempo.
+Grunt, Gulp, Webpack, Browserify, Parcel e outras. 
+Mas agora os módulos são nativos. */
