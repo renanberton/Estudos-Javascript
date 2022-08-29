1a1ceb1cd2a4a458c2539cb0cd51c621dfc571ad
renanberton <renan_berton@yahoo.com.br>
2022-08-29 11:15:17 -0300

    Modify: Alterações nos modules do projeto

--- Estudos-Javascript/Parte 5/Efeitos no DOM/modules/script.js
+++ Estudos-Javascript/Parte 5/Efeitos no DOM/modules/script.js
@@ -14,3 +14,45 @@ Nativo no ES6+
 Ferramentas que permitem dividirmos o código em módulos já existem a bastante tempo.
 Grunt, Gulp, Webpack, Browserify, Parcel e outras. 
 Mas agora os módulos são nativos. */
+
+// Os módulos consistem em separar por exemplo uma função que cria 1 accordion, em um script separado
+// E importar esse script para ser usado em algum local, semelhante ao modo que usamos no React
+
+/* NAMED EXPORTS
+Você pode exportar mais de um valor. 
+Quando for importar utilize as chaves para especificar cada valor. 
+O nome importado deve ser igual ao exportado.
+
+// arquivo scroll.js
+export function scrollSuave() {
+  ...
+};
+export function scrollAnimacao() {
+  ...
+};
+
+// arquivo script.js
+import { scrollSuave, scrollAnimacao } from './scroll.js';
+scrollSuave();
+scrollAnimacao();
+
+// Importe todos os valores em um objeto
+import * as scroll from './scroll.js';
+scroll.scrollSuave();
+scroll.scrollAnimacao(); */
+
+/* VALORES DO EXPORT
+Podemos exportar objetos, funções, classes, números, strings e mais.
+
+// arquivo configuracao.js
+export function scrollSuave() {};
+export const ano = 2000;
+export const obj = {nome: 'Ford'};
+export const str = 'Frase';
+export class Carro {};
+
+// arquivo script.js
+import * as conf from './configuracao.js';
+conf.str;
+conf.obj;
+conf.ano; */
